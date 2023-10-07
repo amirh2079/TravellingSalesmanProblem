a256eeae02b850ae9c933b884ea0feb80e9db79a
amirh2079 <44643894+amirh2079@users.noreply.github.com>
2023-10-07 08:11:44 +0330

    ant colony optimization on tsp problem
    Matplotlib and python 3 are needed

---
+++ data/test.py
@@ -0,0 +1,9 @@
+import os
+
+f = open(os.path.expanduser("~/Programming/AzadUni/ZweiteSemester/ant-colony-tsp-master/data/chn31.txt"))
+s = open(os.path.expanduser("~/Programming/AzadUni/ZweiteSemester/ant-colony-tsp-master/data/chn144.txt"))
+d = open(os.path.expanduser("~/Programming/AzadUni/ZweiteSemester/ant-colony-tsp-master/data/att48.txt"))
+
+print(f.read())
+print(s.read())
+print(d.read())
